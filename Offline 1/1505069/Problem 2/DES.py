9429064f31992e76a1aabf90e6b5b6c8946531a9
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2019-05-10 18:46:57 +0600

    Finished offline 1/problem 2 and starting coding for offline 1/problem1

--- Offline 1/1505069/Problem 2/DES.py
+++ Offline 1/1505069/Problem 2/DES.py
@@ -1,11 +1,12 @@
-import  Convertion
+import  Conversion
 
 # taking input for key and plain text
 #key = input("Enter key : ")
 #plaintext = input("Enter plain text : ")
 
-key="megabuck"
-plaintext="Hello world"
+key="cse_buet"
+plaintext="Attack_at_dawn!"
+
 # Encryption
 
 # padding with ~ to make it divisible by 8
@@ -20,37 +21,46 @@ chunks=length//8
 cipher_text=""
 
 # convert key to bit
-key_in_64bits=Convertion.tobits(key)
+key_in_64bits=Conversion.tobits(key)
+
 # convert key into 56 bits
-key_in_56bits=Convertion.to56bits(key_in_64bits)
+key_in_56bits=Conversion.to56bits(key_in_64bits)
+
+
+SHIFT = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
+keys=[]
+for j in range(0,16):
+    # get first and second half after shifting
+    firsthalf_from_key = Conversion.getfirsthalf(key_in_56bits, SHIFT[j])
+    secondhalf_from_key = Conversion.getsecondhalf(key_in_56bits, SHIFT[j])
+    current_key = []
+    for k in range(0,28):
+        current_key.append(firsthalf_from_key[k])
+    for k in range(0, 28):
+        current_key.append(secondhalf_from_key[k])
+
+    # convert key into 48 bits
+    current_key = Conversion.keyto48bits(current_key)
+    keys.append(current_key)
+
 
 for i in range(0 , chunks) :
     # Group the plaintext by 64 bits
     current_text = plaintext[i*8:(i+1)*8]
-    current_text_in_bits = Convertion.tobits(current_text)
+    current_text_in_bits = Conversion.tobits(current_text)
 
     # tranposed current text
-    input_text = Convertion.Initial_Transpose(current_text_in_bits)
+    input_text = Conversion.Initial_Transpose(current_text_in_bits)
 
-    SHIFT = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
+
     for j in range(0,16):
-        # get first and second half after shifting
-        firsthalf_from_key = Convertion.getfirsthalf(key_in_56bits,SHIFT[j])
-        secondhalf_from_key = Convertion.getsecondhalf(key_in_56bits,SHIFT[j])
-        current_key = []
-        for k in range(0,28):
-            current_key.append(firsthalf_from_key[k])
-        for k in range(0, 28):
-            current_key.append(secondhalf_from_key[k])
 
-        # convert key into 48 bits
-        current_key = Convertion.keyto48bits(current_key)
-
-        left_from_text = Convertion.getfirsthalftext(input_text)
-        right_from_text = Convertion.getsecondhalftext(input_text)
+        current_key=keys[j]
+        left_from_text = Conversion.getfirsthalftext(input_text)
+        right_from_text = Conversion.getsecondhalftext(input_text)
 
         #expanding data to 48 bits
-        expanded_right_from_text = Convertion.expandbits(right_from_text)
+        expanded_right_from_text = Conversion.expandbits(right_from_text)
 
         #xoring current key and expanded right bit
         Xored_bit = []
@@ -59,10 +69,10 @@ for i in range(0 , chunks) :
 
         # sampling 32 bits
 
-        sampledbit=Convertion.samplebit(Xored_bit)
+        sampledbit=Conversion.samplebit(Xored_bit)
 
         # simulated by P-box
-        simulatedbit=Convertion.PBox(sampledbit)
+        simulatedbit=Conversion.PBox(sampledbit)
 
 
         righ_output = []
@@ -84,7 +94,66 @@ for i in range(0 , chunks) :
         swapbit.append(input_text[j+32])
     for j in range(0,32):
         swapbit.append(input_text[j])
-    cipherbit=Convertion.Final_Transpose(swapbit)
+    cipherbit=Conversion.Final_Transpose(swapbit)
 
-    cipher_text+=Convertion.frombits(cipherbit)
-print(cipher_text)
+    cipher_text+=Conversion.frombits(cipherbit)
+print("Ciphered : "+cipher_text)
+
+#Decryption
+newplaintext=""
+
+for i in range(0 , chunks) :
+    # Group the plaintext by 64 bits
+    current_text = cipher_text[i*8:(i+1)*8]
+    current_text_in_bits = Conversion.tobits(current_text)
+
+    # tranposed current text
+    input_text = Conversion.Initial_Transpose(current_text_in_bits)
+
+
+    for j in range(0,16):
+
+        current_key=keys[16-j-1]
+        left_from_text = Conversion.getfirsthalftext(input_text)
+        right_from_text = Conversion.getsecondhalftext(input_text)
+
+        #expanding data to 48 bits
+        expanded_right_from_text = Conversion.expandbits(right_from_text)
+
+        #xoring current key and expanded right bit
+        Xored_bit = []
+        for k in range(0,48):
+            Xored_bit.append(current_key[k]^expanded_right_from_text[k])
+
+        # sampling 32 bits
+
+        sampledbit=Conversion.samplebit(Xored_bit)
+
+        # simulated by P-box
+        simulatedbit=Conversion.PBox(sampledbit)
+
+
+        righ_output = []
+        for k in range(0,32):
+            righ_output.append(left_from_text[k]^simulatedbit[k])
+
+        newinput_text = []
+        for k in range(0,32):
+            newinput_text.append(right_from_text[k])
+        for k in range(0,32):
+            newinput_text.append(righ_output[k])
+
+        input_text=newinput_text
+       # print(input_text)
+
+    swapbit = []
+    for j in range(0,32):
+        swapbit.append(input_text[j+32])
+    for j in range(0,32):
+        swapbit.append(input_text[j])
+    cipherbit=Conversion.Final_Transpose(swapbit)
+
+    newplaintext+=Conversion.frombits(cipherbit)
+
+newplaintext=newplaintext[0:-remain]
+print("Deciphered : "+newplaintext)
