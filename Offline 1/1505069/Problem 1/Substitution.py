8d7576634a0d3adc88c8e44c5b90cf5c29939485
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2019-05-11 20:00:46 +0600

    Finished offline 1

--- Offline 1/1505069/Problem 1/Substitution.py
+++ Offline 1/1505069/Problem 1/Substitution.py
@@ -1,70 +1,97 @@
 import operator
-import re
 
 def change_char(s, p, r):
     return s[:p] + r + s[p + 1:]
+for fileno in range (1,91):
+    if fileno<=60 and fileno>=31:
+        continue
+    filename = "Input/substitution-" + str(fileno) + ".txt"
 
+    file = open(filename, "r")
 
-CipherdText = "QHNABKZTAPNDGJOENAYNOIANLTIBJAUZYBTVYZDVTNYTYBTQHNABEZJOYENVNIZMYBTNUNRZOANOINYHNOYJANVTNYWNVYBTACETVUNOJANOTMETVJUTOYJOHNPZVNYZVSQHNABYTNUAWJYBPNYUNONOIDSPZVKYZVTAYZVTYBTYJUTHJOT"
-highfrechar = ['a','t','e']
-wordsintext = ["atlantis", "experiment", "laboratory", "timeline","flash","goes","back","and","super"]
+    CipherdText = file.readline()
+    CipherdText = CipherdText[0:-1]
 
-charfreq = {}
-for i in range(0,26):
-    x=chr(i+65)
-    charfreq[x]=0
+    file.readline()
+    highchar = file.readline()
+    highchar = highchar[0:-1]
+    highfrechar = highchar.split(", ")
 
-lenght = len(CipherdText)
+    file.readline()
+    words = file.readline()
+    words = words[0:-1]
+    wordsintext = words.split(", ")
+    # wordsintext = ["atlantis", "experiment", "laboratory", "timeline","flash","goes","back","and","super"]
 
-for i in range(0,lenght):
-    charfreq[CipherdText[i]]=charfreq[CipherdText[i]]+1
+    charfreq = {}
+    for i in range(0, 26):
+        x = chr(i + 65)
+        charfreq[x] = 0
 
-sortedcharfreq = sorted(charfreq.items(), key=operator.itemgetter(1))
-sortedcharfreq.reverse()
+    lenght = len(CipherdText)
 
-orginalcharmap = {}
-charsequence = []
-markedchar = []
+    for i in range(0, lenght):
+        charfreq[CipherdText[i]] = charfreq[CipherdText[i]] + 1
 
-for i in range(0,26):
-   charsequence.append(sortedcharfreq[i][0])
-for i in range(0,3):
-    orginalcharmap[charsequence[i]] = highfrechar[i]
-    markedchar.append(highfrechar[i])
-    CipherdText = CipherdText.replace(charsequence[i],highfrechar[i])
+    sortedcharfreq = sorted(charfreq.items(), key=operator.itemgetter(1))
+    sortedcharfreq.reverse()
 
-for i in range(0,len(wordsintext)):
+    orginalcharmap = {}
+    charsequence = []
+    markedchar = []
 
-    currentstring = wordsintext[i]
+    for i in range(0, 26):
+        charsequence.append(sortedcharfreq[i][0])
+    for i in range(0, len(highfrechar)):
+        orginalcharmap[charsequence[i]] = highfrechar[i]
+        markedchar.append(highfrechar[i])
+        CipherdText = CipherdText.replace(charsequence[i], highfrechar[i])
 
-    pattern=""
-    for j in range(0,len(currentstring)):
-        pattern+="#"
-    lenght1 = len(pattern)
-    # making pattern
-    for j in range(0,len(markedchar)):
-        for k in range(0,lenght1):
-            if currentstring[k] == markedchar[j] :
-                pattern = change_char(pattern,k,markedchar[j])
+    for i in range(0, len(wordsintext)):
 
-    for j in range(0,lenght-lenght1):
-        ok = 1
-        for k in range(0,lenght1):
-            if pattern[k] == '#':
-                continue
-            if pattern[k] !=  CipherdText[j+k] :
-                ok = 0
+        currentstring = wordsintext[i]
 
-        if ok == 1:
+        pattern = ""
+        for j in range(0, len(currentstring)):
+            pattern += "#"
+        lenght1 = len(pattern)
+        # making pattern
+        for j in range(0, len(markedchar)):
+            for k in range(0, lenght1):
+                if currentstring[k] == markedchar[j]:
+                    pattern = change_char(pattern, k, markedchar[j])
+
+        for j in range(0, lenght - lenght1):
+            ok = 1
             for k in range(0, lenght1):
                 if pattern[k] == '#':
-                    if CipherdText[j+k] not in markedchar :
-                        orginalcharmap[CipherdText[j+k]] = currentstring[k]
-                        markedchar.append(currentstring[k])
-                        CipherdText = CipherdText.replace(CipherdText[j+k], currentstring[k])
+                    continue
+                if pattern[k] != CipherdText[j + k]:
+                    ok = 0
+
+            if ok == 1:
+                for k in range(0, lenght1):
+                    if pattern[k] == '#':
+                        if CipherdText[j + k] not in markedchar:
+                            orginalcharmap[CipherdText[j + k]] = currentstring[k]
+                            markedchar.append(currentstring[k])
+                            CipherdText = CipherdText.replace(CipherdText[j + k], currentstring[k])
+
+    file.close()
+    filename = "Output/substitution-" + str(fileno) + ".txt"
+    file = open(filename, "w")
+    file.write(CipherdText)
+    file.write("\n\n")
+    for k, v in orginalcharmap.items():
+        file.write(str(k) + ' >>> ' + str(v) + '\n')
+    file.write("\n")
+    accuracy = (((len(orginalcharmap)) / 26.0) * 100.0)
+    file.write("Accuracy : " + str(accuracy))
+    file.write("\n")
+    file.close()
 
 
-print(CipherdText)
+
 
 
 
